618f4f80d2681c84c219ae5eb13d026db302ed62
Gary <58466698+garypach@users.noreply.github.com>
2022-02-21 18:42:04 -0800

    initial setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends ('layouts.main')
+
+@section('content')
+    <h1>hello</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('pages/home');
+});
+
+Route::get('/movie', function () {
+    return view('welcome');
+});
+
+Route::get('/showtimes', function () {
+    return view('welcome');
+});
+
+Route::get('/seats', function () {
+    return view('welcome');
+});
+
+Route::get('/tickets', function () {
+    return view('welcome');
+});
+
+Route::get('/checkout', function () {
+    return view('welcome');
+});
+
+Route::get('/thankyou', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::get('/register', function () {
+    return view('welcome');
+});
+
+Route::get('/account/saved', function () {
     return view('welcome');
 });
